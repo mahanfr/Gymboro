89a72f8fd593c420601de105cb8107d3696f252c
abolfazlssamini <abolfazl.samini78@gmail.com>
2025-05-03 23:34:07 +0330

    added new workout page and selector && fixed the weird error or dev server startup

--- components/ExerciseSetItem.tsx
+++ components/ExerciseSetItem.tsx
@@ -20,14 +20,12 @@ export default function ExerciseSetItem(props: IProps) {
     <View style={styles.container}>
       <ThemedText style={styles.x}>{props.id + 1}</ThemedText>
       <ThemedText style={styles.flexChild}>
-        <Text>{props.lastRep}x{props.lastWeight}Kg</Text>
+        <Text>
+          {props.lastRep}x{props.lastWeight}Kg
+        </Text>
       </ThemedText>
       <View style={styles.button}>
-        <Button
-          title="-"
-          color="#000"
-          onPress={() => props.onUpdate(props.id, rep - 1, weight)}
-        />
+        <Button title="-" color="#000" onPress={() => props.onUpdate(props.id, rep - 1, weight)} />
         <TextInput
           style={[styles.exerciseValueInput]}
           value={props.rep.toString()}
@@ -36,18 +34,10 @@ export default function ExerciseSetItem(props: IProps) {
           }}
           keyboardType="numeric"
         />
-        <Button
-          title="+"
-          color="#000"
-          onPress={() => props.onUpdate(props.id, rep + 1, weight)}
-        />
+        <Button title="+" color="#000" onPress={() => props.onUpdate(props.id, rep + 1, weight)} />
       </View>
       <View style={styles.button}>
-        <Button
-          title="-"
-          color="#000"
-          onPress={() => props.onUpdate(props.id, rep, weight - 1)}
-        />
+        <Button title="-" color="#000" onPress={() => props.onUpdate(props.id, rep, weight - 1)} />
         <TextInput
           style={[styles.exerciseValueInput]}
           value={weight.toString()}
@@ -56,29 +46,12 @@ export default function ExerciseSetItem(props: IProps) {
           }}
           keyboardType="numeric"
         />
-        <Button
-          title="+"
-          color="#000"
-          onPress={() => props.onUpdate(props.id, rep, weight + 1)}
-        />
+        <Button title="+" color="#000" onPress={() => props.onUpdate(props.id, rep, weight + 1)} />
       </View>
       <View style={styles.x}>
         <Text onPress={() => props.onDestroy(props.id)}>
-          <Svg
-            data-slot="icon"
-            fill="none"
-            stroke-width="1.5"
-            stroke="white"
-            width={30}
-            height={30}
-            viewBox="0 0 24 24"
-            aria-hidden={true}
-          >
-            <Path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M6 18 18 6M6 6l12 12"
-            ></Path>
+          <Svg data-slot="icon" fill="none" strokeWidth="1.5" stroke="white" width={30} height={30} viewBox="0 0 24 24" aria-hidden={true}>
+            <Path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12"></Path>
           </Svg>
         </Text>
       </View>
--- components/ThemedView.tsx
+++ components/ThemedView.tsx
@@ -1,14 +1,26 @@
-import { View, type ViewProps } from 'react-native';
+import { View, type ViewProps, StyleSheet } from "react-native";
 
-import { useThemeColor } from '@/hooks/useThemeColor';
+import { useThemeColor } from "@/hooks/useThemeColor";
+import { Colors } from "../constants/Colors";
 
 export type ThemedViewProps = ViewProps & {
-  lightColor?: string;
-  darkColor?: string;
+  lightMode?: boolean;
 };
 
-export function ThemedView({ style, lightColor, darkColor, ...otherProps }: ThemedViewProps) {
-  const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
+// export function ThemedView({ style, lightColor, darkColor, ...otherProps }: ThemedViewProps) {
+//   const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, "background");
 
-  return <View style={[{ backgroundColor }, style]} {...otherProps} />;
+//   return <View style={[{ backgroundColor }, style]} {...otherProps} />;
+// }
+export function ThemedView({ style, lightMode = true, ...otherProps }: ThemedViewProps) {
+  const backgroundColor = lightMode ? Colors.light.background : Colors.dark.background;
+  const borderColor = lightMode ? Colors.light.borderColor : Colors.dark.borderColor;
+  const styles = StyleSheet.create({
+    style: {
+      backgroundColor: backgroundColor,
+      borderColor: borderColor,
+    },
+  });
+
+  return <View style={[styles.style, style]} {...otherProps} />;
 }
