264c7990392d8990a66a0b85bb2304f87ba042f3
mahanfr <mahan.farzaneh@gmail.com>
2025-05-12 21:19:30 +0330

    creating a muscle graph

--- components/MuscleGraph.tsx
+++ components/MuscleGraph.tsx
@@ -11,7 +11,7 @@ export default function MuscleGraph(props: IProps) {
   const activator = new MusclesActivation();
   return (
     <ThemedView style={styles.flex}>
-      <MuscleBack style={styles.size} />
+      <MuscleBack style={styles.size} activator={activator} />
       <MuscleFront style={styles.size} activator={activator} />
     </ThemedView>
   );
@@ -32,16 +32,12 @@ const styles = StyleSheet.create({
 
 export class MusclesActivation {
   chest = 0;
-  upper_pectoralis_major = 0;
-  lower_pectoralis_major = 0;
-  pectoralis_minor = 0;
 
   shin = 0;
   tibialis_anterior = 0;
   extensor_digitorum_longus = 0;
 
   traps = 0;
-  levator_scapulae = 0;
   upper_trapezius = 0;
   middle_trapezius = 0;
   lower_trapezius = 0;
@@ -49,12 +45,12 @@ export class MusclesActivation {
   biceps = 0;
   biceps_short_head = 0;
   biceps_long_head = 0;
+  brachialis = 0;
 
   triceps = 0;
-  triceps_brachii_lateral_head = 0;
-  triceps_brachii_long_head = 0;
-  triceps_brachii_medial_head = 0;
-  brachialis = 0;
+  triceps_lateral_head = 0;
+  triceps_long_head = 0;
+  triceps_medial_head = 0;
 
   forearms = 0;
   brachioradialis = 0;
@@ -63,92 +59,50 @@ export class MusclesActivation {
 
   abs = 0;
   obliques = 0;
-  rectus_abdominis = 0;
 
   adductors = 0;
-  adductor_brevis = 0;
-  adductor_longus = 0;
-  gracilis = 0;
-  adductor_magnus = 0;
 
   abductors = 0;
-  pectineus = 0;
-  sartorius = 0;
 
   quadriceps = 0;
   vastus_lateralis = 0;
   vastus_intermedius = 0;
-  rectus_femoris = 0;
   vastus_medialis = 0;
 
   calves = 0;
-  gastrocnemius_medial_head = 0;
-  gastrocnemius_lateral_head = 0;
-  soleus = 0;
-  plantaris = 0;
-  popliteus = 0;
 
   shoulders = 0;
   anterior_deltoid = 0;
   lateral_deltoid = 0;
   posterior_deltoid = 0;
-  infraspinatus = 0;
-  supraspinatus = 0;
 
   upper_back = 0;
   serratus_anterior = 0;
-  teres_minor = 0;
-  teres_major = 0;
-  rhomboids = 0;
+  teres = 0;
+  infraspinatus = 0;
 
-  lower_back = 0;
-  latissimus_dorsi = 0;
-  erector_spinae = 0;
+  lats = 0;
 
   glutes = 0;
-  gluteus_medius = 0;
-  gluteus_minimus = 0;
-  gluteus_maximus = 0;
 
   hamstrings = 0;
   biceps_femoris = 0;
   semitendinosus = 0;
-  semimembranosus = 0;
 
-  public getColor(muscle: string): string {
-    switch (muscle) {
-      case "chest":
-        return "#003f5c";
-      case "shin":
-        return "#58508d";
-      case "traps":
-        return "#8a508f";
-      case "biceps":
-        return "#bc5090";
-      case "triceps":
-        return "#de5a79";
-      case "forearms":
-        return "#ff6361";
-      case "abs":
-        return "#ff8531";
-      case "adductors":
-        return "#ffa600";
-      case "abductors":
-        return "#003f5c";
-      case "quadriceps":
-        return "#58508d";
-      case "calves":
-        return "#8a508f";
-      case "shoulders":
-        return "#bc5090";
-      case "upper_back":
-        return "#ff6361";
-      case "glutes":
-        return "#de5a79";
-      case "hamstrings":
-        return "#ff8531";
-      case "lower_back":
-        return "#ffa600";
+  public getColor(activation_value: number): string {
+    switch (activation_value) {
+      case 0:
+        return "#ffe4ea";
+      case 1:
+        return "#ff91ab";
+      case 2:
+        return "#ef6c6e";
+      case 3:
+        return "#ff2357";
+      case 4:
+        return "#80122c";
+      case 5:
+        return "#400916";
       default:
         return "#bebebe";
     }
